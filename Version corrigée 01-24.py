4ca6f759160165a276b6da482f918efa4272fe51
Mathias3303 <93943668+Mathias3303@users.noreply.github.com>
2022-01-25 21:39:48 +0100

    fusion des modifications du 24 et fin de l'inertie

--- Version corrigée 01-24.py
+++ Version corrigée 01-24.py
@@ -1,21 +1,32 @@
 import random
+from random import randint
 import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
-from math import pi,exp
+from math import pi,exp,cos
 import matplotlib.colors as couleurs
 
-
+### COMMENTAIRES
 
 #FB Il y a parfois une petite erreur très subtile dans le mouvement des fourmis (rarement !). La verrez-vous ? =D
 
 #FB Pour le moment, ça fait des fourmis qui sortent de la fourmilière et qui se promènenent au hasard et qui récupèrent parfois de la nourriture au hasard... De plus tout ça se fait sur un exemple pour le moment, à généraliser
-
-## Variables globales techniques
-
 #FB Je vais modifier les codes ci-dessous pour vous permettre d'avoir un code couleur précis, JE VOUS EXPLIQUERAI COMMENT CA MARCHE.
 
 #FB Les deux variables suivantes vont permettre de dessiner le monde avec des couleurs définies :
+
+
+
+
+
+
+
+
+
+### VARIABLES GLOBALES
+
+## pour l'affichage graphique
+
 
 # blanc pour les cases vides
 # gris pour les obstacles
@@ -38,28 +49,57 @@ CODE_RAU = 2
 CODE_FOURMILIERE = 3
 
 
+## pour l'arrêt ou la poursuite de la simulation
+
 NB_RAU_INITIAL = 3 #ici, test manuel ; à implémenter dans remplit_RAU_au_hasard
 NB_RAU_COLLECTEES = 0 #idee booleen compteur de RAU rentrées à la maison qui acheve le programme (while), cf. dernière fonction
 
+SIMULATION_EN_COURS = True # cette variable indique que la simulation est en cours (tant qu'elle vaut True, la boucle principale continue - elle devient False par exemple si on ferme la fenêtre grâce à un bout de code que je vous ai mis en cadeau bonus !)
 
+
+## liées à l'interprétation de l'inertie
+
+EPSILON_COS = 0.001
 INFLUENCE_COS_0 = 5
 INFLUENCE_COS_PId4 = 3
 INFLUENCE_COS_PId2 = 2
 INFLUENCE_COS_3PId4 = 1
 INFLUENCE_COS_PI = 0
 
+
+VECTEUR_VITESSE_FOURMIS_SANS_RAU = [(1,1), (-1,1), (1,0)]
+VECTEUR_VITESSE_FOURMIS_AVEC_RAU = []
+
+
+ALPHA_INERTIE = 10 #variable caractérisant la préférence au déplacement (même chose avec ALPHA_PHEROMONE)
+
+
 INFLUENCE_CASE = 1 #à supprimer ? ancien chemin pour le déplacement choisi
 
 
-SIMULATION_EN_COURS = True # cette variable indique que la simulation est en cours (tant qu'elle vaut True, la boucle principale continue - elle devient False par exemple si on ferme la fenêtre grâce à un bout de code que je vous ai mis en cadeau bonus !)
+## liées à l'interprétation de l'inertie
 
-## UN EXEMPLE à généraliser
+ALPHA_PHEROMONE = 'à faire'
+
+
+
+
+
+
+
+
+
+### DEFINITION DE L'ENVIRONNEMENT
+
+
+
+
 
 # ENVIRONNEMENT : rectangle de maxX*maxY cases pratiquables
 
 maxX = 10
 maxY = 10
-# taille FIXEE dans cet exemple
+# taille FIXEE dans cet exemple (à généraliser avec un environnement généré au hasard)
 
 ENVIRONNEMENT = np.zeros((maxX+2,maxY+2))
 
@@ -83,19 +123,30 @@ FOURMIS_SANS_RAU = [(5,5),(5,5),(5,5)]
 FOURMIS_AVEC_RAU = []
 
 
-ALPHA_DEPLACEMENT = 1 #variable caractérisant la préférence au déplacement
-VECTEUR_VITESSE_FOURMIS_AVEC_RAU = [(1,1), (2,1), (1,0)] #à supprimer ? ancien chemin pour le déplacement choisi
 
-
-TEMPS_PAUSE = 1 #nani ?
+TEMPS_PAUSE = 1 # à quoi ça sert ?
 
 
 
+# mettre plus tard : remplit_RAU_au_hasard(x) , remplit_obstacles_au_hasard(x)
 
-## Mise en place aléatoire de l'environnement
 
-#FB Je mets le code suivant dans une fonction pour que vous compreniez ce que je veux dire :
 
+
+
+
+
+
+
+
+
+
+
+
+### FONCTIONS UTILITAIRES
+
+
+## fonctions liées à l'environnement
 
 def remplit_RAU_au_hasard(nbUnites) :
     '''
@@ -105,10 +156,6 @@ def remplit_RAU_au_hasard(nbUnites) :
     global NB_RAU_INITIAL
 
     NB_RAU_INITIAL = nbUnites
-
-    #FB je remplace le input par un paramètre dans ma fonction
-
-    #FB l'algo que vous utilisez ci-dessous ne fait pas ce que vous voulez de manière sure : si par hasard il tombe deux fois sur la même case, ou à un moment sur une case obstacle, à la fin vous aurez moins de cases RAU que vous le vouliez... à améliorer...
 
     Nb_RAU_placees = 0
 
@@ -124,28 +171,23 @@ def remplit_RAU_au_hasard(nbUnites) :
     else:
         print('Pas assez de place dans E pour autant de RAU')
 
-#FB Faire de même la fonction suivante :
 
 def remplit_obstacles_au_hasard(nbObstacles) :
     '''
     met nbObstacles obstacles dans la variable ENVIRONNEMENT
     '''
 
-    #FB A faire
+    # à insérer sur github
 
 
 
-
-
-## fonctions utilitaires
-
-#FB le but de la fonction suivante est peu clair : vu son usage plus loin, il me semble qu'il s'agit de donner les cases voisines SANS OBSTACLES. Mais vous n'enlevez pas les obstacles... dans ce cas il faut la modifier (je le fais)
+## liste des voisins praticables
 
 def liste_des_voisins_praticables(tableau,case):
     '''Renvoie liste des coordonnees des cases accessibles, c'est ) dire sur le tableau et sans obstacle'''
     i = case[0]
     j = case[1]
-    voisins_potentiels = [(i-1,j-1),(i-1,j),(i-1,j+1),(i,j+1),(i+1,j+1),(i+1,j),(i+1,j-1),(i,j-1)]
+    voisins_potentiels = [(i-1,j-1),(i-1,j),(i-1,j+1),(i,j+1),(i+1,j+1),(i+1,j),(i+1,j-1),(i,j-1)] # parcours d'en haut à gauche, dans le sens horaire
     indice_voisins_praticables = []
     for candidat in voisins_potentiels:
         if (0 <= candidat[0] < tableau.shape[0]) and (0 <= candidat[1] < tableau.shape[1]) and (tableau[candidat] != CODE_OBSTACLE ):
@@ -153,115 +195,133 @@ def liste_des_voisins_praticables(tableau,case):
     return indice_voisins_praticables
 
 
-## Deplacement aléatoire, contraint par les obstacles, d'une fourmi de "caseAvant" à "caseAprès"
 
-#FB Je reformate ça plus bas dans la partie ## Deplacement des fourmis : une fonction générique qui fait une boucle sur les fourmis et qui gère notamment la transformation des fourmis sans RAU en fourmis avec RAU, et une fonction qui fait le déplacement d'une fourmi à la fois.
-
-#FB Je laisse votre code en commentaire ci-dessous, comparez avec ce que j'ai fait :
-
-'''
-def deplacement_aleatoire() :
-    for k in range(len(FOURMIS_AVEC_RAU)) :
-        L = liste_des_voisins_praticables(ENVIRONNEMENT, FOURMIS_AVEC_RAU[k])
-        nb_hasard = randint(0, len(L)-1)
-        FOURMIS_AVEC_RAU[k] = L[nb_hasard]
-    for k in range(len(FOURMIS_SANS_RAU)) :
-        L = liste_des_voisins_praticables(ENVIRONNEMENT, FOURMIS_SANS_RAU[k])
-        nb_hasard = randint(0, len(L)-1)
-        FOURMIS_SANS_RAU[k] = L[nb_hasard]
-'''
-
-## Attractivité de l'environnement pour la fourmi active
-
-
-#FB je ne regarde pas les deux fonctions suivantes, car je ne sais pas exactement ce que vous voulez faire (pas de docstring...)
-
-#FB Avec le setting graphique que je vous ai mis, vous allez pouvoir les tester vous-même et voir si ça fait ce que vous voulez.
+## fonctions de calculs
 
 def couple_delta(case_suivante , case_actuelle):
-    '''renvoie un vecteur vitesse instantanée entre 2 cases traversées successives'''
+    '''
+    renvoie un vecteur vitesse instantanée entre 2 cases traversées successives
+    '''
     di = case_suivante[0] - case_actuelle[0]
     dj = case_suivante[1] - case_actuelle[1]
     couple_delta = ( di , dj )
     return couple_delta
 
 
-def cos(delta_actuel, delta_possible) :
+def cos_des_deltas(delta_actuel, delta_possible) :
     produit_scalaire = np.dot(delta_actuel, delta_possible) #produit scalaire entre vecteur avant et vecteur suivant
     norme_vecteur_possible =( (delta_possible[0])**2 + (delta_possible[1])**2 )**(1/2) #norme du vecteur vitesse qui pointe vers une case possible
     norme_vecteur_actuel = ( (delta_actuel[0])**2 + (delta_actuel[1])**2 )**(1/2)
     cos_case_possible = produit_scalaire / (norme_vecteur_possible*norme_vecteur_actuel) #on divise par la norme pour isoler cos
+
+
+    # arrondissement des valeurs de cos obtenues :
+
+    if abs(cos_case_possible - cos(0)) <= EPSILON_COS :
+        cos_case_possible = cos(0)
+
+    elif abs(cos_case_possible - cos(pi/4)) <= EPSILON_COS :
+        cos_case_possible = cos(pi/4)
+
+    elif abs(cos_case_possible - cos(pi/2)) <= EPSILON_COS :
+        cos_case_possible = cos(pi/2)
+
+    elif abs(cos_case_possible - cos(3*pi/4)) <= EPSILON_COS :
+        cos_case_possible = cos(3*pi/4)
+
+    elif abs(cos_case_possible - cos(pi)) <= EPSILON_COS :
+        cos_case_possible = cos(pi)
+
     return cos_case_possible
 
-##def poids_deplacement_une_fourmi(fourmi):
-    #!!!! CORRECTION NECESSAIRE !!!!
-    global ALPHA_DEPLACEMENT
+
+
+
+
+### FONCTIONS D'ATTRACTIVITE
+
+## pour l'inertie
+
+def attractivite_inertie_une_fourmi(fourmi):
+
+
+    global ALPHA_INERTIE
 
     case_actuelle = fourmi
-    delta_actuel = VECTEUR_VITESSE_FOURMIS_AVEC_RAU[k] #couple_delta de l'actuel
+    delta_actuel = VECTEUR_VITESSE_FOURMIS_SANS_RAU[0] #couple_delta de l'actuel ATTENTION NE TRAITE QU'UN GROUPE DE FOURMIS
     voisins_praticables = liste_des_voisins_praticables(ENVIRONNEMENT , case_actuelle)
     L_cos_voisins_praticables = []
-    L_attractivite_des_voisins_praticables = []
+    L_attractivite_inertie_des_voisins_praticables = []
 
 
     for case_possible in voisins_praticables :
         delta_possible = couple_delta(case_possible, case_actuelle) #vecteur vitesse de case actuelle à case possible
-
+        cos_case_possible = cos_des_deltas(delta_actuel, delta_possible)
 
         L_cos_voisins_praticables.append(cos_case_possible) #liste des cos de l'angle formé par les vecteurs actuel et possible
-        print(L_cos_voisins_praticables)
 
-    for cos in L_cos_voisins_praticables :
-        L_attractivite_des_voisins_praticables.append(exp(ALPHA_DEPLACEMENT*cos))
+    for x in L_cos_voisins_praticables :
+        if x == cos(0) :
+            L_attractivite_inertie_des_voisins_praticables.append(INFLUENCE_COS_0)
 
-    print(L_attractivite)
-##
+        elif x == cos(pi/4) :
+            L_attractivite_inertie_des_voisins_praticables.append(INFLUENCE_COS_PId4)
+
+        elif x == cos(pi/2) :
+            L_attractivite_inertie_des_voisins_praticables.append(INFLUENCE_COS_PId2)
+
+        elif x == cos(3*pi/4) :
+            L_attractivite_inertie_des_voisins_praticables.append(INFLUENCE_COS_3PId4)
+
+        elif x == cos(pi) :
+            L_attractivite_inertie_des_voisins_praticables.append(INFLUENCE_COS_PI)
+
+    return L_attractivite_inertie_des_voisins_praticables
 
 
-def poids_deplacement_des_fourmis():
 
-    global ALPHA_DEPLACEMENT
 
-    for k in range(len(FOURMIS_AVEC_RAU)):
-        case_actuelle = FOURMIS_AVEC_RAU[k]
-        delta_actuel = VECTEUR_VITESSE_FOURMIS_AVEC_RAU[k] #couple_delta de l'actuel
-        voisins_praticables = liste_des_voisins_praticables(ENVIRONNEMENT , case_actuelle)
-        L_cos_voisins_praticables = []
-        L_attractivite_des_voisins_praticables = []
-        norme_vecteur_actuel = ( (delta_actuel[0])**2 + (delta_actuel[1])**2 )**(1/2)
+## pour les phéromones environnants
 
-        for case_possible in voisins_praticables :
-            delta_possible = couple_delta(case_possible, case_actuelle) #vecteur vitesse de case actuelle à case possible
-            produit_scalaire = np.dot(delta_actuel, delta_possible) #produit scalaire entre vecteur avant et vecteur suivant
-            norme_vecteur_possible =( (delta_possible[0])**2 + (delta_possible[1])**2 )**(1/2) #norme du vecteur vitesse qui pointe vers une case possible
-            cos_case_possible = produit_scalaire / (norme_vecteur_possible*norme_vecteur_actuel) #on divise par la norme pour isoler cos
-
-            L_cos_voisins_praticables.append(cos_case_possible) #liste des cos de l'angle formé par les vecteurs actuel et possible
-            print(L_cos_voisins_praticables)
-
-    for cos in L_cos_voisins_praticables :
-        L_attractivite_des_voisins_praticables.append(exp(ALPHA_DEPLACEMENT*cos))
-
-    print(L_attractivite)
+#def attractivite_pheromones_une_fourmi():  A FAIRE
 
 
 
 
 
 
+### FONCTIONS DE DEPLACEMENT
 
-#def poids_deplacement_des_fourmis():
+
+def deplacement_inertie_et_pheromones_une_fourmi(fourmi):
+    '''
+    chaque fourmi se déplace selon son inertie et les phéromones alentours
+    '''
+
+    cases_possibles = liste_des_voisins_praticables(ENVIRONNEMENT,fourmi)
+
+    attractivite_inertie = attractivite_inertie_une_fourmi(fourmi)
+    #attractivite_pheromones = attractivite_pheromones_une_fourmi(fourmi)
+
+    attractivite_inertie_softmax = [exp(ALPHA_INERTIE*x) for x in attractivite_inertie] # ajouter même chose avec pheromones
+
+    nouvelle_case = random.choices(cases_possibles , attractivite_inertie_softmax ) # choix pondéré par les attractivités ATTENTION C'EST UN ARRAY
+    print(nouvelle_case)
+
+    return nouvelle_case[0]
 
 
-## Deplacement des fourmis
+
+
+
 
 def deplacement_aleatoire_une_fourmi(fourmi) :
     '''
-    pour le moment, chaque fourmi se promène aléatoirement
+    chaque fourmi se promène aléatoirement
     '''
-    nouvelle_pos = random.choice(liste_des_voisins_praticables(ENVIRONNEMENT,fourmi))
-    print("la fourmi", fourmi,"va en ", nouvelle_pos)
-    return nouvelle_pos
+    nouvelle_case = random.choice(liste_des_voisins_praticables(ENVIRONNEMENT,fourmi))
+    print("la fourmi", fourmi,"va en ", nouvelle_case)
+    return nouvelle_case
 
 
 
@@ -269,7 +329,7 @@ def deplacement_aleatoire_une_fourmi(fourmi) :
 def deplacement_des_fourmis() :
 
     '''
-    gère le déplacement de toutes les fourmis, et le changemetn d'état entre "sans RAU" et "avec RAU"
+    gère le déplacement de toutes les fourmis, et le passage entre "sans RAU" et "avec RAU"
     '''
 
     global NB_RAU_COLLECTEES
@@ -280,7 +340,7 @@ def deplacement_des_fourmis() :
     L_provisoire_SANS_RAU = []
 
     for fourmi in FOURMIS_AVEC_RAU:
-        new_position = deplacement_aleatoire_une_fourmi(fourmi)
+        new_position = deplacement_aleatoire_une_fourmi(fourmi) # on peut remplacer 'aleatoire' par 'inertie_et_pheromones' et inversement
         if ENVIRONNEMENT[new_position] == CODE_FOURMILIERE :
             print( "la fourmi", fourmi, "a rempli son quota")
             NB_RAU_COLLECTEES += 1
@@ -289,7 +349,7 @@ def deplacement_des_fourmis() :
 
 
     for fourmi in FOURMIS_SANS_RAU :
-        new_position = deplacement_aleatoire_une_fourmi(fourmi)
+        new_position = deplacement_inertie_et_pheromones_une_fourmi(fourmi) # on peut remplacer 'aleatoire' par 'inertie_et_pheromones' et inversement
         if ENVIRONNEMENT[new_position] == CODE_RAU :
             print( "la fourmi",fourmi,"a récupéré de la bouffe en", new_position)
             L_provisoire_AVEC_RAU.append(new_position)
@@ -304,6 +364,9 @@ def deplacement_des_fourmis() :
 
 
 
+
+
+### GESTION ET AFFICHAGE SIMULATION
 
 ## Affichage graphique et interaction
 
@@ -337,7 +400,11 @@ def affichage_graphique() :
     plt.draw()
 
 
+
+
+
 ## Initialisation de Matplotlib et Boucle Principale
+
 
 fig, ax = plt.subplots()
 
